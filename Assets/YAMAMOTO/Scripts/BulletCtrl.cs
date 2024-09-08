8f6aac40eb293703a5171891a4aa58c5b37a19f2
s1f102402091 <s1F102402091@iniad.org>
2024-09-08 15:08:16 +0900

    射撃システム: 弾の複製
    Bulletを左クリックで発射できるようになりました。

---
+++ Assets/YAMAMOTO/Scripts/BulletCtrl.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCtrl : MonoBehaviour
+{
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+    }
+
+    //Start():読み込み時１回のみ実行される.
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        //Shoot(new Vector3(0, 200, 2000));
+    }
+
+    //Update():フレームごとに一回実行される.
+    void Update()
+    {
+        
+    }
+}
