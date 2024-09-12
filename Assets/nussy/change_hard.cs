b8617ca7bd6f2e1e1eaf5922a78dbac33094e723
s1f102202156 <107388505+s1f102202156@users.noreply.github.com>
2024-09-12 19:06:59 +0900

    atageselect_content_open

--- Assets/nussy/change_hard.cs
+++ Assets/nussy/change_hard.cs
@@ -9,6 +9,7 @@ public class change_hard : MonoBehaviour
     public ScoreManager scoreManager;
     private int score = 0;
     public int limitscore = 30000;
+    public GameObject chane, locked;
 
     public void Start()
     {
@@ -21,6 +22,8 @@ public class change_hard : MonoBehaviour
         {
             score = 0;
         }
+        chane = GameObject.Find("chane_ha");
+        locked = GameObject.Find("lock_ha");
     }
     public void change_button() //change_button‚Æ‚¢‚¤–¼‘O‚É‚µ‚Ü‚·
     {
--- Assets/nussy/change_midium.cs
+++ Assets/nussy/change_midium.cs
@@ -9,9 +9,13 @@ public class change_midium : MonoBehaviour
     public ScoreManager scoreManager;
     private int score = 0;
     public int limitscore = 20000;
+    public GameObject chane, locked;
+    
 
     public void Start()
     {
+        chane = GameObject.Find("chane_mi");
+        locked = GameObject.Find("lock_mi");
         scoreManager = GetComponent<ScoreManager>();
         try
         {
@@ -19,6 +23,11 @@ public class change_midium : MonoBehaviour
         }
         catch { 
             score = 0;
+        }
+        if (score >= limitscore)
+        {
+            Destroy(chane);
+            Destroy(locked);
         }
     }
     public void change_button() //change_button‚Æ‚¢‚¤–¼‘O‚É‚µ‚Ü‚·
