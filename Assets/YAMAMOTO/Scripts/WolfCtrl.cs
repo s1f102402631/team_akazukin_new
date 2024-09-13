9e8a129343583d9b06910dcc6aac111336d9a0d4
s1F10210372 <85667177+s1F10210372@users.noreply.github.com>
2024-09-13 15:09:24 +0900

    Wolfに当たり判定,倒した時のスコア表示を付与
    Assets＞Targetに色々付与したWolfが入っています。

--- Assets/YAMAMOTO/Scripts/WolfCtrl.cs
+++ Assets/YAMAMOTO/Scripts/WolfCtrl.cs
@@ -7,8 +7,18 @@ public class WolfCtrl : MonoBehaviour
     public GameObject ObjScoreManager;
     public ScoreManager ScrScoreManager;
 
+    [SerializeField]
+    private GameObject PointObj; //Point
+    [SerializeField]
+    private GameObject PosObj; //的
+    [SerializeField]
+    private Vector3 AdjPos; //ダメージ表示の位置調整
+
+    private int Number = 200; //ポイント数
+
     void OnCollisionEnter(Collision collision)
     {
+        ViewPoint(Number);
         Destroy(this.gameObject, 0.0f);
         ScrScoreManager.PlusScore("Wolf");
         ScrScoreManager.DebugScore();
@@ -25,5 +35,13 @@ public class WolfCtrl : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void ViewPoint(int _damage) //的の頭上にスコアを表示
+    {
+        string pointText = $"+{_damage}";
+        GameObject _pointObj = Instantiate(PointObj); //プレハブをインスタンス化
+        _pointObj.GetComponent<TextMesh>().text = pointText; //数値を入れる
+        _pointObj.transform.position = PosObj.transform.position + AdjPos; //ダメージ表示オブジェクトの位置を設定
     }
 }
