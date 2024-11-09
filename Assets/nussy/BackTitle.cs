11746529512cd4a387f46cf250a10497b8619e50
s1f102202156 <s1f102202156@iniad.org>
2024-11-09 13:03:58 +0900

    終

---
+++ Assets/nussy/BackTitle.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class BackTitle : MonoBehaviour
+{
+
+    public void change_button()
+    {
+            SceneManager.LoadScene("Top scene");
+        }
+}
+
